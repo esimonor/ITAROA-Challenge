1e10c522ac3bb4b27cd4085cb8907248c8165d10
esimonor <esimonor18dw@ikzubirimanteo.com>
2020-10-22 11:15:51 +0200

    added form view

---
+++ resources/views/create_product.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+    <!--POPPER
+        <script src="https://unpkg.com/@popperjs/core@2"></script>
+    POPPER-->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Itaroa</title>
+        <link rel="icon" href="{{url('img/logo-composer-transparent3.png')}}"type="image/png" sizes="16x16">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Alef" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Arsenal' rel='stylesheet'>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
+    <body>
+        <!-- Navbar -->
+        <nav class="navbar sticky-top navbar-light bg-light">
+            <img width="10%" class="d-inline-block align-top" alt="" loading="lazy" src="img/logo.png">
+            <a>Shops</a>
+            <a>Restaurants</a>
+            <a>Contact</a>
+            <a>Languaje</a>
+        </nav>
+
+        <!-- Banner -->
+        <div class="banner">
+        </div>
+
+        <!--Form-->
+        <form>
+            <span>Product name:</span><input type="text">
+            <span>Product description:</span><input type="text">
+            <span>Product price:</span><input type="number" step="0.01">
+            <span>Product stock:</span><input type="number">
+            <span>Product image:</span><input type="file">
+            <span>Product link:</span><input type="text">
+        </form>
+    <footer class="footer">
+      <p>ITAROA &copy 2020</p>
+    </footer>
+    
+    <!-- JQUERY, POPPER, BOOTSTRAP-->
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
