655d1f467abad26dca0a592093ffc77750404ed0
esimonor <esimonor18dw@ikzubirimanteo.com>
2020-10-28 09:58:48 +0100

    Added some coments

--- resources/views/establishment.blade.php
+++ resources/views/establishment.blade.php
@@ -20,7 +20,7 @@
     <body>
         <!-- Navbar -->
         <nav class="navbar sticky-top navbar-light bg-light">
-            <img width="10%" class="d-inline-block align-top" alt="" loading="lazy" src="img/logo.png">
+            <img width="10%" class="d-inline-block align-top" alt="" loading="lazy" src="{{URL::asset('img/logo.png')}}">
             <a>Shops</a>
             <a>Restaurants</a>
             <a>Contact</a>
